073d380fbcfa0a45297644ca340672ecff1c3800
camcd <48787654+camcd@users.noreply.github.com>
2019-05-06 08:45:51 +0800

    Add files via upload

---
+++ Visualizing Attention.py
@@ -0,0 +1,47 @@
+from __future__ import unicode_literals, print_function, division
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
+import matplotlib.ticker as ticker
+import torch
+import Train as T
+import pylab
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+output_words, attentions = T.evaluate(
+    T.encoder1, T.attn_decoder1, "je suis trop froid .")
+plt.matshow(attentions.numpy())
+
+def showAttention(input_sentence, output_words, attentions):
+    # Set up figure with colorbar
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(attentions.numpy(), cmap='bone')
+    fig.colorbar(cax)
+
+    # Set up axes
+    ax.set_xticklabels([''] + input_sentence.split(' ') +
+                       ['<EOS>'], rotation=90)
+    ax.set_yticklabels([''] + output_words)
+
+    # Show label at every tick
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.show()
+    pylab.show()
+
+def evaluateAndShowAttention(input_sentence):
+    output_words, attentions = T.evaluate(
+        T.encoder1, T.attn_decoder1, input_sentence)
+    print('input =', input_sentence)
+    print('output =', ' '.join(output_words))
+    showAttention(input_sentence, output_words, attentions)
+
+
+evaluateAndShowAttention("elle a cinq ans de moins que moi .")
+
+evaluateAndShowAttention("elle est trop petit .")
+
+evaluateAndShowAttention("je ne crains pas de mourir .")
+
+evaluateAndShowAttention("c est un jeune directeur plein de talent .")
