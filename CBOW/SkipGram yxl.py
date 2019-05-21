efc39446599b0aebc4843a7cccead4db31f05666
尹小丽 <yxl746816994@163.com>
2019-05-21 15:05:58 -0500

    CBOW and SkipGram

--- CBOW/SkipGram yxl.py
+++ CBOW/SkipGram yxl.py
@@ -4,9 +4,11 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import torch.optim as optim
 
-CONTEXT_SIZE = 2
+
+CONTEXT_SIZE = 3
+TARGET_SIZE=1
 #raw_text = open('leta.en',encoding='utf-8').\
-#        read().split()
+#        read().strip().split()
 raw_text = """Motorists have also been asked to avoid the area around the complex where more than 39,000 employees work - about one third of BASF's total global workforce.
 Scientists' manifesto hoped to help government realize potential of innovations for economic development RIGA, Oct 17 (LETA) - Scientists and academicians are planning to submit to Prime Minister Maris Kucinskis (Greens/Farmers) a manifesto explaining that scientists, in close cooperation with businessmen, can create innovations contributing to the economic development of Latvia.
 Scientists hope that the manifesto will help the government realize how scientific innovations reach businessmen and contribute to the economic growth of the country, Latvian Academy of Sciences President Ojars Sparitis emphasized in an interview with the LNT television this morning.
@@ -38,42 +40,46 @@ Phil Collins announces European comeback tour London, Oct 17, 2016 (AFP) - Veter
 # By deriving a set from `raw_text`, we deduplicate the array
 vocab = set(raw_text)
 
-word_to_ix = {word:i for i, word in enumerate(vocab)}
+word_to_ix = {word: i for i, word in enumerate(vocab)}
 # enumerate() 函数用于将一个可遍历的数据对象(如列表、元组或字符串)组合为一个索引序列
 data = []
-for i in range(2,len(raw_text)-2):
-    context = [raw_text[i-2],raw_text[i-1],raw_text[i+1],raw_text[i+2]]
+for i in range(2, len(raw_text) - 2):
     target = raw_text[i]
-    data.append((context,target))
+    context = [raw_text[i - 2], raw_text[i - 1], raw_text[i + 1], raw_text[i + 2]]
+    data.append((target,context))
 print(data[:5])
 
-class CBOW(nn.Module):
-    def __init__(self, n_word, n_dim, context_size):
-        super(CBOW,self).__init__()
-        self.embeddings = nn.Embedding(n_word,n_dim)
-        self.linear1 = nn.Linear(2*context_size*n_dim,128)
-        self.linear2 = nn.Linear(128,n_word)
-    def forward(self,inputs):
-        embeds = self.embeddings(inputs).view((1,-1))
+
+class SkipGram(nn.Module):
+    def __init__(self, n_word, n_dim,target_size):
+        super(SkipGram, self).__init__()
+        #实例化
+        self.embeddings = nn.Embedding(n_word, n_dim)  #embedding把输入的词变为向量
+        self.linear1 = nn.Linear(50, 128)
+        self.linear2 = nn.Linear(128, n_word)
+
+    def forward(self, inputs):
+        embeds = self.embeddings(inputs).view((4, -1))#view(1,-1)是把字典转化到一行
         out = F.relu(self.linear1(embeds))
         out = self.linear2(out)
-        log_probs = F.log_softmax(out,dim=1)
+        log_probs = F.log_softmax(out, dim=1)
         return log_probs
 
-model = CBOW(len(word_to_ix),100,CONTEXT_SIZE)
+
+model = SkipGram(len(word_to_ix), 200, TARGET_SIZE)
 losses = []
 loss_function = nn.NLLLoss()
-optimizer = optim.SGD(model.parameters(),lr=0.001)
+optimizer = optim.SGD(model.parameters(), lr=0.001)
 
-for epoch in range(7):
+for epoch in range(100):
     print('epoch{}'.format(epoch))
     print('*'*10)
     total_loss = 0
-    for context, target in data:
-        context = Variable(torch.LongTensor([word_to_ix[i] for i in context]))
+    for target,context, in data:
         target = Variable(torch.LongTensor([word_to_ix[target]]))
-        out = model(context)
-        loss = loss_function(out,target)
+        context = Variable(torch.LongTensor([word_to_ix[i] for i in context]))
+        out = model(target)
+        loss = loss_function(out, context)
         total_loss += loss.item()
         optimizer.zero_grad()
         loss.backward()
